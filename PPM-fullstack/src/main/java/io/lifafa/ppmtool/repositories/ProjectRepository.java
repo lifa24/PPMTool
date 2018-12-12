19457bd6c35c98a5546b676622bbd7a17fda0e42
Faxiang Li <lifa0917@gmail.com>
2018-12-12 12:15:31 -0500

    add the repository interface

---
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package io.lifafa.ppmtool.repositories;
+
+import io.lifafa.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
