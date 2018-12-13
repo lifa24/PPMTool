839dee14fe538eb2649a10bbf8fbd7a0c78b061e
Faxiang Li <lifa0917@gmail.com>
2018-12-13 09:38:09 -0500

    Refactor project controller

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/web/ProjectController.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package io.lifafa.ppmtool.web;
 
 import io.lifafa.ppmtool.domain.Project;
+import io.lifafa.ppmtool.services.MapVaildationErrorService;
 import io.lifafa.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,18 +24,13 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapVaildationErrorService mapVaildationErrorService;
+
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if(result.hasErrors()){
-
-            Map<String,String> errorMap = new HashMap<>();
-
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-
-            return new ResponseEntity<Map<String,String>>(errorMap,HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> errorMap = mapVaildationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
 
         Project project1= projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
