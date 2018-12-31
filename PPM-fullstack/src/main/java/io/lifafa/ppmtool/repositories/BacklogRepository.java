d1ba3b6052e25b370512eb0e60f28d0fac234f9f
Faxiang Li <lifa0917@gmail.com>
2018-12-31 10:19:54 -0500

    Backlog and ProjectTask Entities

---
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,9 @@
+package io.lifafa.ppmtool.repositories;
+
+import io.lifafa.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog,Long> {
+}
---
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package io.lifafa.ppmtool.repositories;
+
+import io.lifafa.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask,Long> {
+}
