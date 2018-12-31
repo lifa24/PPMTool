6396e69c125b781b6867b7b3f6bd97d60c24ba8d
Faxiang Li <lifa0917@gmail.com>
2018-12-31 10:55:15 -0500

    Entity Relationship:Project and Backlog

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/domain/Backlog.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/domain/Backlog.java
@@ -1,9 +1,8 @@
 package io.lifafa.ppmtool.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 
 @Entity
 public class Backlog {
@@ -14,6 +13,10 @@ public class Backlog {
     private String projectIdentifier;
 
     //oneToOne with project
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="project_id",nullable = false)
+    @JsonIgnore
+    private Project project;
 
     //oneTo Many projectTasks
 
@@ -39,6 +42,14 @@ public class Backlog {
 
     public String getProjectIdentifier() {
         return projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
     }
 
     public void setProjectIdentifier(String projectIdentifier) {
--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectService.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package io.lifafa.ppmtool.services;
 
+import io.lifafa.ppmtool.domain.Backlog;
 import io.lifafa.ppmtool.domain.Project;
 import io.lifafa.ppmtool.exceptions.ProjectIdException;
+import io.lifafa.ppmtool.repositories.BacklogRepository;
 import io.lifafa.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +13,24 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            if(project.getId()==null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            if(project.getId()!=null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
             return projectRepository.save(project);
 
         }catch(Exception e){
