cbbdc1025323d5f0597774739174cf8faee4fa15
Faxiang Li <lifa0917@gmail.com>
2018-12-31 22:36:54 -0500

    create project task

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectTaskService.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,9 @@
 package io.lifafa.ppmtool.services;
 
+import io.lifafa.ppmtool.domain.Backlog;
 import io.lifafa.ppmtool.domain.ProjectTask;
 import io.lifafa.ppmtool.repositories.BacklogRepository;
-import io.lifafa.ppmtool.repositories.ProjectRepository;
+import io.lifafa.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +14,34 @@ public class ProjectTaskService {
     private BacklogRepository backlogRepository;
 
     @Autowired
-    private ProjectRepository projectRepository;
+    private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
         //PTs to be added to a specfic project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
         //set the bl to pt
+        projectTask.setBacklog(backlog);
 
         //we want our project sequence to be like this: IDPRO-1 IDPRO-2 ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+
         //Update the BL SEQUENCE
+        BacklogSequence++;
+
+        //Add Sequence to Project Task
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         //INITIAL priority when priority null
-        //INITIAL status when status is null
+        //if(projectTask.getPriority()==0||projectTask.getPriority()==null){
+        //    projectTask.setPriority(3);
+        //}
 
-    }
+        //INITIAL status when status is null
+        if(projectTask.getStatus()=="" || projectTask.getStatus()==null){
+            projectTask.setStatus("TO_DO");
+        }
+        return projectTaskRepository.save(projectTask);    }
 
 }
---
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/web/BacklogController.java
@@ -0,0 +1,33 @@
+package io.lifafa.ppmtool.web;
+
+import io.lifafa.ppmtool.domain.ProjectTask;
+import io.lifafa.ppmtool.services.MapVaildationErrorService;
+import io.lifafa.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapVaildationErrorService mapVaildationErrorService;
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            BindingResult result,
+                                            @PathVariable String backlog_id){
+        ResponseEntity<?> erroMap = mapVaildationErrorService.MapValidationService(result);
+        if(erroMap != null) return erroMap;
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id,projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
