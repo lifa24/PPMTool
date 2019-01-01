6a8b5300c71c65294125174689243e01c81ef880
Faxiang Li <lifa0917@gmail.com>
2019-01-01 10:31:35 -0500

    Find ProjectTask by projectSequence

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectTaskService.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/ProjectTaskService.java
@@ -72,4 +72,9 @@ public class ProjectTaskService {
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 
+    public ProjectTask findByProjectSequence(String backlog_id,String pt_id){
+
+        return projectTaskRepository.findByProjectSequence(pt_id);
+    }
+
 }
--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/web/BacklogController.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/web/BacklogController.java
@@ -39,4 +39,12 @@ public class BacklogController {
 
     }
 
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id,
+                                            @PathVariable String pt_id){
+
+        ProjectTask projectTask = projectTaskService.findByProjectSequence(backlog_id,pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask,HttpStatus.OK);
+    }
+
 }
