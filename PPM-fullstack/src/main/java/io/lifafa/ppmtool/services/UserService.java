3000b3aac379b7d4b84d2628fe5fd85694ae39cc
Faxiang Li <lifa0917@gmail.com>
2019-01-06 21:31:02 -0500

    user registration-1

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/UserService.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/services/UserService.java
@@ -1,11 +1,25 @@
 package io.lifafa.ppmtool.services;
 
+import io.lifafa.ppmtool.domain.User;
 import io.lifafa.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        //Username has to unique(exception)
+        //make sure that password and confirmPassword match
+        //we don't persist or show the confirmPassword
+        return userRepository.save(newUser);
+    }
 }
