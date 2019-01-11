213af95b5e704c44b2fdf1b58ff4c068e0537128
Faxiang Li <lifa0917@gmail.com>
2019-01-10 20:46:31 -0500

    set up one to many relationship between user and projects

--- PPM-fullstack/src/main/java/io/lifafa/ppmtool/domain/Project.java
+++ PPM-fullstack/src/main/java/io/lifafa/ppmtool/domain/Project.java
@@ -41,6 +41,12 @@ public class Project {
     @JsonIgnore
     private Backlog backlog;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private User user;
+
+    private String projectLeader;
+
     public Project() {
     }
 
@@ -114,6 +120,14 @@ public class Project {
 
     public void setBacklog(Backlog backlog) {
         this.backlog = backlog;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @PrePersist
